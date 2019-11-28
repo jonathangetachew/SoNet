65590115f486b533cda08e3d844ad6cfcfe2dfdd
mahmoud reda <mahmoudreda400@gmail.com>
2019-11-27 18:41:39 -0600

    first commit

---
+++ src/main/java/edu/mum/sonet/SonotApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.sonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonotApplication.class, args);
+	}
+
+}
