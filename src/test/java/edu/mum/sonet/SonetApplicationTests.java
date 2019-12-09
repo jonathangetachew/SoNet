09496dc338e57732324205c1f2dedc8540d448da
Jonathan Tuta <jonathangetachew@gmail.com>
2019-12-08 20:07:45 -0600

    Disabled tests

--- src/test/java/edu/mum/sonet/SonetApplicationTests.java
+++ src/test/java/edu/mum/sonet/SonetApplicationTests.java
@@ -1,8 +1,10 @@
 package edu.mum.sonet;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class SonetApplicationTests {
 
